ae5efc3cf30169afb192e5b4779cd3bf98f9a915
Yuri Na <nyryn0945@gmail.com>
2018-05-01 11:18:58 +0900

    ..

--- night/src/main/webapp/resources/js/lastPath.js
+++ night/src/main/webapp/resources/js/lastPath.js
@@ -26,32 +26,19 @@ $(document).ready(function() {
 	
 	$.fn.select2.defaults.set("theme", "bootstrap");
 	$("#statnFnm").select2({
-		ajax: {
-			url: "api/subway/getSubwayList",
-			dataType: "json",
-			processResults: function(data) {
-				return {
-					results: data
-				};
-			},
-			cache : true
-		},
+		data: subwayList,
 		placeholder: "지하철 선택"
 	})
 	.on('select2:select', function (e) {
 		var data = e.params.data;
-		console.log(data);
 		$.ajax({
-			url: "api/subway/SearchLastTrainTimeByIDService",
-			data: {
-				station_cd: data.id
-			},
+			url: "api/path/searchLastTrainTimeByIDService/" + data.id,
 			type: "GET",
 			dataType: "JSON"
 		})
 		.done(function(res) {
-			console.log(res);
-			// buildBusRoute(res);
+			//console.log(res);
+			buildLastSubway(res);
 		})
 		.fail(function(err) {
 			console.log(err);
@@ -60,7 +47,7 @@ $(document).ready(function() {
 	
 	$("#busRouteNm").select2({
 		ajax: {
-			url: "api/bus/getBusRouteList",
+			url: "api/path/getBusRouteList",
 			dataType: "json",
 			minimumInputLength: 2,
 			processResults: function(data) {
@@ -73,10 +60,7 @@ $(document).ready(function() {
 	}).on('select2:select', function (e) {
 		var data = e.params.data;
 		$.ajax({
-			url: "api/bus/getStaionByRoute",
-			data: {
-				busRouteId: data.id
-			},
+			url: "api/path/getStaionByRoute/" + data.id,
 			type: "GET",
 			dataType: "JSON"
 		})
@@ -90,20 +74,39 @@ $(document).ready(function() {
 	});
 });
 
+var buildLastSubway = function(res) {
+	var $wrap = $('#searchLastTrainTimeByID');
+	$wrap.removeClass('mb-0').addClass('mb-3').html('');
+	$.each(res, function(i, arr) {
+		$('<li>', {'class': 'list-group-item list-group-item-action list-group-item-secondary font-weight-bold'})
+		.text(i == 0 ? '상행/내선' : '하행/외선')
+		.appendTo($wrap);
+		$.each(arr, function(i2, item) {
+			$('<li>', {'class': 'list-group-item list-group-item-action'})
+			.append(
+				$('<div>', {class: 'subName'}).append(
+					$('<span>', {class: 'text-primary mr-2'}).text('종점'),
+					$('<span>').text(item.SUBWAYENAME)				
+				),
+				$('<div>', {class: 'time'}).append(
+					$('<span>', {class: 'text-primary mr-2'}).text('출발시간'),
+					$('<span>').text(item.LEFTTIME)				
+				)
+			)
+			.appendTo($wrap);
+		});
+	});
+};
+
 var buildBusRoute = function(res) {
 	var $wrap = $('#getStaionByRoute');
 	$wrap.removeClass('mb-0').addClass('mb-3').html(''); // 내용 초기화
 	$.each(res, function(i, item) {
-		$('<li>', {
-			'class': 'list-group-item list-group-item-action',
-			'data-toggle': 'modal',
-			'data-target': '#busStationInfoModal'
-		})
+		$('<li>', {'class': 'list-group-item list-group-item-action'})
 		.append(
 			$('<span>').text(item.stationNm),
-			item.lastTm == ':' ? "" : $('<time>').text(item.lastTm)
+			item.lastTm == ':' ? "" : $('<time>', {class: 'text-primary ml-2'}).text(item.lastTm)
 		)
-		.data('data', item)
 		.appendTo($wrap);
 	});
 };
