b2a82844a852abbd24a4ce14d1c25d2e55900db1
nyryn0945 <nyryn0945@gmail.com>
2018-05-01 14:52:57 +0900

    180501

--- night/src/main/webapp/resources/js/lastPath.js
+++ night/src/main/webapp/resources/js/lastPath.js
@@ -23,6 +23,23 @@ $(document).ready(function() {
 	// $("div").greenify({
 	// color : "orange"
 	// });
+	$('.findLastQuickPath').submit(function(e) {
+		e.preventDefault();
+		var $this = $(this);
+		$.ajax({
+			// url: 'api/path/test',
+			url: 'api/path/findLastQuickPath',
+			data: $this.serialize(),
+			type: 'GET',
+			dataType: 'JSON'
+		})
+		.done(function(res) {
+			buildPath(res.paths);
+		})
+		.fail(function(err) {
+			console.log(err);
+		});
+	});
 	
 	$.fn.select2.defaults.set("theme", "bootstrap");
 	$("#statnFnm").select2({
@@ -74,9 +91,45 @@ $(document).ready(function() {
 	});
 });
 
+var buildPath = function(path) {
+	var $wrap = $('#findLastQuickPath');
+	$wrap.removeClass('mb-0').addClass('mt-3').html('');
+	
+	$.each(path, function(i, item) {
+		$('<li>', {'class': 'list-group-item list-group-item-secondary'})
+		.append(
+			$('<h6>', {class: 'float-left'}).text((i + 1) + '번째 루트'),
+			$('<small>', {class: 'float-right'}).text('도보 ' + Math.round(item.walking / 60) + '분 / 총 ' + Math.round(item.duration / 60) + '분')
+		)
+		.appendTo($wrap);
+		var trans = item.transit;
+		$.each(trans, function(i2, item2) {
+			var len1 = item2.departure_time.length;
+			var len2 = item2.arrival_time.length;
+			$('<li>', {'class': 'list-group-item'})
+			.append(
+				$('<div>', {class: 'subName'}).append(
+					$('<span>').text(item2.departure_stop),
+					$('<small>', {class: 'text-muted'}).text(item2.departure_time.substring(len1 - 9, len1 - 3)),
+					$('<i class="fas fa-long-arrow-alt-right text-primary ml-2 mr-2"></i>'),
+					$('<span>').text(item2.arrival_stop),
+					$('<small>', {class: 'text-muted'}).text(item2.arrival_time.substring(len2 - 9, len2 - 3)),
+				),
+				$('<div>', {class: 'time'}).append(
+					$('<small>', {class: 'text-primary mr-2'}).text(item2.type == 'BUS' ? '버스' : '지하철'),
+					$('<small>').text(item2.short_name),
+					$('<small>', {class: 'text-primary ml-3 mr-2'}).text('소요시간'),
+					$('<small>').text(Math.round(item2.duration / 60) + '분')				
+				)
+			)
+			.appendTo($wrap);
+		});
+	});
+};
+
 var buildLastSubway = function(res) {
 	var $wrap = $('#searchLastTrainTimeByID');
-	$wrap.removeClass('mb-0').addClass('mb-3').html('');
+	$wrap.removeClass('mb-0').html('');
 	$.each(res, function(i, arr) {
 		$('<li>', {'class': 'list-group-item list-group-item-action list-group-item-secondary font-weight-bold'})
 		.text(i == 0 ? '상행/내선' : '하행/외선')
@@ -100,7 +153,7 @@ var buildLastSubway = function(res) {
 
 var buildBusRoute = function(res) {
 	var $wrap = $('#getStaionByRoute');
-	$wrap.removeClass('mb-0').addClass('mb-3').html(''); // 내용 초기화
+	$wrap.removeClass('mb-0').html(''); // 내용 초기화
 	$.each(res, function(i, item) {
 		$('<li>', {'class': 'list-group-item list-group-item-action'})
 		.append(
--- node/routes/api/path/index.js
+++ node/routes/api/path/index.js
@@ -5,10 +5,114 @@ const urlencode = require('urlencode');
 const request = require('request');
 const convert = require('xml-js');
 const googleMapsClient = require('@google/maps').createClient({
-	key: "AIzaSyBlXEl9ShyPbgaAKl7R2LDg2DiOByz81q0",
+	key: "AIzaSyBNOXW6jN3IaO-u2gFywBBrRHkQ1z2CIxk",
 	Promise: Promise
 });
 const models = require('../../../models/mysql');
+
+router.get('/test', (req, res) => {
+	res.json({"paths":[{"transit":[{"type":"BUS","short_name":"33","duration":2955,"departure_stop":"신분당선강남역","arrival_stop":"위례그린파크푸르지오","departure_time":"2018-5-2 00:05:51","arrival_time":"2018-5-2 00:55:06"}],"walking":496,"duration":4351},{"transit":[{"type":"BUS","short_name":"440","duration":2967,"departure_stop":"신분당선강남역","arrival_stop":"위례중앙푸르지오.신안인스빌","departure_time":"2018-5-2 00:21:16","arrival_time":"2018-5-2 01:10:43"}],"walking":1256,"duration":4703},{"transit":[{"type":"BUS","short_name":"452","duration":2541,"departure_stop":"신분당선강남역","arrival_stop":"민속마을.선경아파트","departure_time":"2018-5-2 00:17:45","arrival_time":"2018-5-2 01:00:06"}],"walking":1473,"duration":4494},{"transit":[{"type":"SUBWAY","short_name":"2호선","duration":660,"departure_stop":"강남역","arrival_stop":"잠실","departure_time":"2018-5-2 00:16:30","arrival_time":"2018-5-2 00:27:30"},{"type":"SUBWAY","short_name":"8호선","duration":840,"departure_stop":"잠실역","arrival_stop":"산성","departure_time":"2018-5-2 00:39:00","arrival_time":"2018-5-2 00:53:00"}],"walking":2420,"duration":4188}]});
+});
+
+// localhost/api/path/findLastQuickPath
+router.get('/findLastQuickPath', (req, res) => {
+
+	const spotArr = [req.query.departure_stop, req.query.arrival_stop];
+
+	// 카카오 API로 좌표 탐색
+	const kakaoMapResult = (query) => {
+		var options = {
+			url: "https://dapi.kakao.com/v2/local/search/keyword.json?query=" + urlencode(query),
+			headers: {
+				"Authorization": "KakaoAK 748b29a61c3e145b51f8172294a9ddfd"
+			}
+		};
+
+		return new Promise((resolve, reject) => {
+			request(options, (error, response, body) => {
+				if (!error && response.statusCode == 200) {
+					var json = JSON.parse(body);
+					resolve({
+						x: json.documents[0].x,
+						y: json.documents[0].y
+					});
+				} else {
+					reject(error);
+				}
+			});
+		});
+	};
+
+	// 좌표 구하고 경로 계산
+	Promise.all(spotArr.map(item => kakaoMapResult(item)))
+	.then((kakaoRes) => {
+		var d = new Date();
+		var hour = d.getHours();
+		if (hour > 5) {
+			d.setDate(d.getDate() + 1);
+		}
+		d.setHours(2);
+		d.setMinutes(0);
+		d.setSeconds(0);
+
+		return googleMapsClient.directions({
+			origin: [kakaoRes[0].y, kakaoRes[0].x],
+			destination: [kakaoRes[1].y, kakaoRes[1].x],
+			alternatives: true,
+			mode: 'transit',
+			language: 'ko',
+			arrival_time: d
+		}).asPromise();
+	})
+	.then((response) => {
+		var paths = [];
+		var routes = response.json.routes;
+		for (var route of routes) {
+			var steps = route.legs[0].steps;
+			var tempObj = {
+				transit: [],
+				walking: 0,
+				duration: route.legs[0].duration.value
+			};
+			for (var step of steps) {
+				if (step.travel_mode == "TRANSIT") {
+					var detail = step.transit_details;
+					tempObj.transit.push({
+						type: detail.line.vehicle.type,
+						short_name: detail.line.short_name,
+						duration: step.duration.value,
+						departure_stop: detail.departure_stop.name,
+						arrival_stop: detail.arrival_stop.name,
+						departure_time: new Date(detail.departure_time.value * 1000).toLocaleString("ko-KR", {
+							timeZone: "Asia/Seoul"
+						}),
+						arrival_time: new Date(detail.arrival_time.value * 1000).toLocaleString("ko-KR", {
+							timeZone: "Asia/Seoul"
+						})
+					});
+				} else if (step.travel_mode == "WALKING") {
+					tempObj.walking += step.duration.value;
+				}
+			}
+			paths.push(tempObj);
+		}
+
+		// 도보 시간 오름차순으로 정렬
+		paths.sort((a, b) => {
+			return a.walking - b.walking;
+		});
+
+		return paths;
+	})
+	.then((paths) => {
+		res.json({
+			paths: paths
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+});
 
 // localhost/api/path/searchLastTrainTimeByIDService/:station_cd
 router.get('/searchLastTrainTimeByIDService/:station_cd', (req, res) => {
